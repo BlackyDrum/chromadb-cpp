c5dc1c75e62a4a8dff4ae8b7126cc21f66885cb6
BlackyDrum <ganiaytan1010@web.de>
2024-06-10 23:18:22 +0200

    Make embedding request method protected

--- include/ChromaDB/Embeddings/EmbeddingFunction.h
+++ include/ChromaDB/Embeddings/EmbeddingFunction.h
@@ -17,14 +17,15 @@ namespace chromadb {
 	public:
 		EmbeddingFunction(const std::string& apiKey, const std::string& model, const std::string& baseUrl, const std::string& path);
 
-		nlohmann::json Request(const nlohmann::json& body);
-
 		virtual std::vector<std::vector<double>> Generate(const std::vector<std::string>& documents) = 0;
 	protected:
 		std::string m_BaseUrl;
 		std::string m_Path;
 		std::string m_ApiKey;
 		std::string m_Model;
+
+	protected:
+		nlohmann::json Request(const nlohmann::json& body);
 	};
 
 } // namespace chromadb
