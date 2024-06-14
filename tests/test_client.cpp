ec996994425b551e88351ff1e4c4290ac00c18d1
BlackyDrum <ganiaytan1010@web.de>
2024-06-14 17:03:17 +0200

    Add test class

--- tests/test_client.cpp
+++ tests/test_client.cpp
@@ -1,8 +1,25 @@
 #include <gtest/gtest.h>
 #include "ChromaDB/ChromaDB.h"
 
-TEST(ClientTest, Initialization) {
-    chromadb::Client client("http", "localhost", "8080");
-    EXPECT_EQ(client.GetTenant(), "default_tenant");
-    EXPECT_EQ(client.GetDatabase(), "default_database");
+using namespace chromadb;
+
+class ClientTest : public ::testing::Test {
+protected:
+    Client* client;
+
+    void SetUp() override {
+        client = new Client("http", "localhost", "8080", "test_database", "test_tenant", "authToken");
+    }
+
+    void TearDown() override {
+        client->DeleteCollections();
+
+        delete client;
+    }
+};
+
+TEST_F(ClientTest, ConstructorInitializesCorrectly)
+{
+    EXPECT_EQ(client->GetDatabase(), "test_database");
+    EXPECT_EQ(client->GetTenant(), "test_tenant");
 }
