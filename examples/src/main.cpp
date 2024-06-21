713eda2bc93634a2655561c3e152caa6cf532b7e
BlackyDrum <ganiaytan1010@web.de>
2024-06-21 22:59:49 +0200

    Remove tabstops from example file

--- examples/src/main.cpp
+++ examples/src/main.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-	try
-	{
-		chromadb::Client client("http", "localhost", "8080");
+    try
+    {
+        chromadb::Client client("http", "localhost", "8080");
 
-		std::cout << "Version: " << client.GetVersion() << std::endl;
-		std::cout << "Database: " << client.GetDatabase() << std::endl;
-		std::cout << "Tenant: " << client.GetTenant() << std::endl;
-	}
-	catch (const chromadb::ChromaException& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+        std::cout << "Version: " << client.GetVersion() << std::endl;
+        std::cout << "Database: " << client.GetDatabase() << std::endl;
+        std::cout << "Tenant: " << client.GetTenant() << std::endl;
+    }
+    catch (const chromadb::ChromaException& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
